48ccf0d1e7a8563123c02017d65611a62fcaef60
Mandela Steele-Dadzie <msteeledadzie@gmail.com>
2023-01-03 02:13:06 -0700

    components updated to functional components

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 
-class Header extends React.Component {
-  render() {
+const Header = () => {
     return (
       <header>
         <h1>RESTy</h1>
       </header>
     );
   }
-}
 
 export default Header;
